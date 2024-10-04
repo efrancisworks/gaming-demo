d3712df834ca5c5312ee52b106afd53b0b1bd346
Emanuel F. <70411130+efrancisworks@users.noreply.github.com>
2024-10-04 01:43:33 -0700

    Inventory in progress

--- rpg/Scripts/GUI/Inventory/Inventory_slot.gd
+++ rpg/Scripts/GUI/Inventory/Inventory_slot.gd
@@ -1,2 +1,34 @@
 class_name InventorySlot
 extends  PanelContainer
+
+@export var type: ItemData.Type
+
+func init(t: ItemData.Type, cms: Vector2) -> void:
+	type = t
+	custom_minimum_size = cms
+	
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	if data is InventoryItem:
+		if type == ItemData.Type.MAIN:
+			if get_child_count() == 0:
+				return true
+			else:
+				if type == data.get_parent().type:
+					return true
+				return get_child(0).data.type == data.data.type
+		else:
+			return data.data.type == type
+	else:
+		return false	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+		if get_child_count() > 0:
+			var item := get_child(0)
+			if item == data:
+				return
+			item.reparent(data.get_parent())
+		data.reparent(self)
+func _physics_process(delta: float) -> void:
+	#checking equipment
+	pass
+func _gui_input(event: InputEvent) -> void:
+	pass
--- rpg/Scripts/GUI/Inventory/inventory_main.gd
+++ rpg/Scripts/GUI/Inventory/inventory_main.gd
@@ -3,3 +3,7 @@ extends Control
 func _ready() -> void:
 	var slot = InventorySlot.new()
 	add_child(slot)
+
+
+func _on_control_gui_input(event: InputEvent) -> void:
+	print("Event")
